811357d179d86bcefdca56cdfcf8eb048ddbd9fc
Diogo Oliveira <diogo.o@icloud.com>
2021-06-03 16:47:43 -0300

    painel administrativo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+            @foreach ($contatos as $contato)
+                <div class="card mt-5">
+                    <div class="card-header">Registrado às {{ date('H:m\h \d\e d-m-Y', strtotime($contato->created_at)) }}</div>
+                    <div class="card-body">
+                        <p><strong>Nome:</strong> {{$contato->name}}</p>
+                        <p><strong>Telefone:</strong> {{$contato->phone}}</p>
+                        <p><strong>Email:</strong> {{$contato->email}}</p>
+                        <p><strong>Mensagem:</strong> {{$contato->message}}</p>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
